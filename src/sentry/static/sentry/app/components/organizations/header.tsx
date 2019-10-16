90bcb267c371610326305b7ea7c9c2d86db86e8e
Danny Lee <dlee@sentry.io>
2019-10-16 11:23:28 -0700

    fixup! ref(ui): Adjusting widths of things in the GlobalSelectionHeader

--- src/sentry/static/sentry/app/components/organizations/header.tsx
+++ src/sentry/static/sentry/app/components/organizations/header.tsx
@@ -2,14 +2,16 @@ import styled from 'react-emotion';
 import {Flex} from 'grid-emotion';
 
 const Header = styled(Flex)`
-  border-bottom: 1px solid ${p => p.theme.borderDark};
-  box-shadow: ${p => p.theme.dropShadowLight};
-  font-size: ${p => p.theme.fontSizeExtraLarge};
-  height: 60px;
-  z-index: ${p => p.theme.zIndex.globalSelectionHeader};
   position: relative;
   width: 100%;
+  height: 60px;
+
+  border-bottom: 1px solid ${p => p.theme.borderDark};
+  box-shadow: ${p => p.theme.dropShadowLight};
+  z-index: ${p => p.theme.zIndex.globalSelectionHeader};
+
   background: #fff;
+  font-size: ${p => p.theme.fontSizeExtraLarge};
 `;
 
 export default Header;
